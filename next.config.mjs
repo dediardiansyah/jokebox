708ea418911302dd7079967e8f724bd906107c24
egi purwana <egipurwana@gmail.com>
2025-11-17 19:23:41 +0700

    test nextjs config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  outputFileTracingIncludes: {
+    "/api/*/": ["./node_modules/.prisma/client/*/"],
+    "/": ["./node_modules/.prisma/client//"],
+  },
 };
 
 export default nextConfig;
