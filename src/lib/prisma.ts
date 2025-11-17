eff44c7213632392bafb4c6e0bebbb9c380d67db
egi purwana <egipurwana@gmail.com>
2025-11-17 18:23:10 +0700

    prisma change

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,13 @@
+// import { PrismaClient } from "@prisma/client";
+// const globalForPrisma = global as unknown as { prisma: PrismaClient };
+// export const prisma = globalForPrisma.prisma || new PrismaClient();
+// // if (process.env.NODE_ENV !== "production")
+// globalForPrisma.prisma = prisma;
+
 import { PrismaClient } from "@prisma/client";
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
-export const prisma = globalForPrisma.prisma || new PrismaClient();
-// if (process.env.NODE_ENV !== "production")
-globalForPrisma.prisma = prisma;
+let prisma;
+if (!global.prisma) {
+  global.prisma = new PrismaClient();
+}
+prisma = global.prisma;
+export default prisma;
