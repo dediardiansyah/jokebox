88effad8b8b9649923314c0b53909310be7795de
egi purwana <egipurwana@gmail.com>
2025-11-17 19:19:24 +0700

    solusi 1 menit

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -4,13 +4,19 @@
 // // if (process.env.NODE_ENV !== "production")
 // globalForPrisma.prisma = prisma;
 
-import { PrismaClient } from "@prisma/client";
-//@ts-ignore
-import { withPg } from '@prisma/adapter-pg';
+// import { PrismaClient } from "@prisma/client";
+// //@ts-ignore
+// import { withPg } from '@prisma/adapter-pg';
 
-let prisma;
-if (!global.prisma) {
-  global.prisma = new PrismaClient();
-}
-prisma = global.prisma;
+// let prisma;
+// if (!global.prisma) {
+//   global.prisma = new PrismaClient();
+// }
+// prisma = global.prisma;
+// export default prisma;
+
+
+import { PrismaClient } from "@prisma/client";
+import { withAccelerate } from "@prisma/extension-accelerate";
+const prisma = new PrismaClient().$extends(withAccelerate());
 export default prisma;
