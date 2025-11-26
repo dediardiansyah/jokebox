63338be3335dfbf9f45a9d6e6ef9646a5ad83e7a
egi purwana <egipurwana@gmail.com>
2025-11-26 16:33:30 +0700

    nextjs config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 
-const path = require("path");
+// const path = require("path");
 
 const nextConfig = {
   /* config options here */
@@ -8,10 +8,10 @@ const nextConfig = {
   //   "/api/*/": ["./node_modules/.prisma/client/*/"],
   //   "/": ["./node_modules/.prisma/client//"],
   // },
-  experimental: {
-    //outputFileTracingRoot gotta get the non package prisma-generated/
-    outputFileTracingRoot: path.join(__dirname, ".."),
-  },
+  // experimental: {
+  //   //outputFileTracingRoot gotta get the non package prisma-generated/
+  //   outputFileTracingRoot: path.join(__dirname, ".."),
+  // },
   // experimental: {
   //   optimizePackageImports: ["@prisma/client"],
   //   serverComponentsExternalPackages: ["@prisma/client", "prisma"],
